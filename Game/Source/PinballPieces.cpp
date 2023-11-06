dae01b2336d048dc6871524227808c9e7f0fad04
Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>
2023-11-06 09:46:00 +0100

    Assets y modulos de pinbalpieces creados
    Falta cargar las texturas
    
    Co-Authored-By: PauMenaTorres <117993994+PauMenaTorres@users.noreply.github.com>

---
+++ Game/Source/PinballPieces.cpp
@@ -0,0 +1,41 @@
+#include "PinballPieces.h"
+
+#include "SDL_image/include/SDL_image.h"
+
+
+PinballPieces::PinballPieces() : Module()
+{
+	
+}
+
+PinballPieces::~PinballPieces() {
+
+}
+
+bool PinballPieces::Awake() 
+{
+
+	
+
+	return true;
+}
+
+bool PinballPieces::Start() 
+{
+
+	
+
+	return true;
+}
+
+bool PinballPieces::Update(float dt)
+{
+	
+	return true;
+}
+
+bool PinballPieces::CleanUp()
+{
+
+	return true;
+}
---
+++ Game/Source/PinballPieces.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Module.h"
+#include "List.h"
+#include "Point.h"
+
+
+
+class PinballPieces : public Module
+{
+public:
+
+	PinballPieces();
+
+	virtual ~PinballPieces();
+
+	bool Awake();
+
+	bool Start();
+
+	bool Update(float dt);
+
+	bool CleanUp();
+
+
+private:
+	SDL_Texture* palaI;
+	SDL_Texture* palaD;
+	SDL_Texture* muelle;
+	SDL_Texture* impulso;
+	SDL_Texture* p10;
+	SDL_Texture* p20;
+	SDL_Texture* p30;
+	SDL_Texture* trianguloD;
+	SDL_Texture* trianguloI;
+	SDL_Texture* cuadrado_webon;
+	SDL_Texture* recta;
+	SDL_Texture* trianguloD;
+	SDL_Texture* estrella;
+	SDL_Texture* u_feliz;
+	SDL_Texture* x2;
+	SDL_Texture* x3;
+	SDL_Texture* x5;
+	SDL_Texture* paredI;
+	SDL_Texture* paredD;
+};
+
+
+
